4809e8162c3f595add27a6051032db83826ecf96
Anusha Mehrin <anusha.mehrin@northsouth.edu>
2021-05-06 05:09:13 +0600

    done

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from news import views
 urlpatterns = [
     path('home', views.home, name='home'),
-    path('theDailyStar',views.theDailyStar, name='theDailyStar'),
+    path('',views.theDailyStar, name='theDailyStar'),
     path('prothomalo',views.prothomAlo, name='prothomalo'),
     path('dhakatribune',views.dhakaTribune, name='dhakatribune'),
     path('bdnews24',views.bdNews24, name='bdnews24')
